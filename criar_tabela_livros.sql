ca8543102fb0ad9bf88920eb0b2e0092dcc326d1
IsaacRamcs <142359173+IsaacRamcs@users.noreply.github.com>
2024-09-25 13:16:19 -0300

    Update criar_tabela_livros.sql

--- criar_tabela_livros.sql
+++ criar_tabela_livros.sql
@@ -5,12 +5,10 @@ USE livros;
 CREATE TABLE livros (
 IDLivro INT PRIMARY KEY AUTO_INCREMENT,
 nomeLivro VARCHAR(100) NOT NULL,
-linguaLivro VARCHAR(100) NOT NULL,
-anoPublicacao YEAR,
-IDCategoria INT,
+categoriaLivro VARCHAR(100) NOT NULL,
+anoPublicacao INT,
 IDAutor INT,
 IDEditora INT,
-CONSTRAINT fk_livros_livros FOREIGN KEY (IDCategoria) REFERENCES categoria(IDCategoria),
 CONSTRAINT fk_livros_autor FOREIGN KEY (IDAutor) REFERENCES autor(IDAutor),
 CONSTRAINT fk_livros_editora FOREIGN KEY (IDEditora) REFERENCES editora(IDEditora)
 );
@@ -18,8 +16,8 @@ CONSTRAINT fk_livros_editora FOREIGN KEY (IDEditora) REFERENCES editora(IDEditor
 CREATE TABLE ratings (
 IDRating INT PRIMARY KEY AUTO_INCREMENT,
 notaRating DECIMAL(3, 2),
-totalRating INT,
-totalReview INT,
+totalRating DOUBLE,
+totalReview DOUBLE,
 IDLivro INT,
 CONSTRAINT fk_ratings_livros FOREIGN KEY (IDLivro) REFERENCES livros(IDLivro)
 );
