192df9f2e1a6291bbfc13b6c58da92ed6800f9b9
Thaboll <127352652+Thaboll@users.noreply.github.com>
2024-09-25 13:42:51 -0300

    Delete criar_tabela_info.sql

--- criar_tabela_info.sql
+++
@@ -1,7 +0,0 @@
- CREATE TABLE info (
- IDInfo INT PRIMARY KEY AUTO_INCREMENT,
- numPaginas INT,
- precoLivro DECIMAL (10, 2),
- IDLivro INT,
- CONSTRAINT fk_info_livros FOREIGN KEY (IDLivro) REFERENCES livros(IDLivro)
- );
