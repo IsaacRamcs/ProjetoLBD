940129a3ece4a3718071f25e62411866b047d2cf
Lfurbs <142275246+Lfurbs@users.noreply.github.com>
2024-09-25 17:00:45 -0300

    Update consulta3.sql
    Consulta de quais livros tem rating acima de 4,5, listando os autores

--- consulta3.sql
+++ consulta3.sql
@@ -1,1 +1,12 @@
-
+SELECT 
+livros.nomeLivro, 
+autor.nomeAutor, 
+ratings.notaRating
+FROM 
+livros
+JOIN 
+autor ON livros.IDAutor = autor.IDAutor
+JOIN 
+ratings ON livros.IDLivro = ratings.IDLivro
+WHERE 
+ratings.notaRating > 4.5;
