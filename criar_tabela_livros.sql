935db24edb781297088b5ef872f0604037b69bbc
Thaboll <127352652+Thaboll@users.noreply.github.com>
2024-09-25 13:41:42 -0300

    Update criar_tabela_livros.sql

--- criar_tabela_livros.sql
+++ criar_tabela_livros.sql
@@ -21,3 +21,11 @@ totalReview DOUBLE,
 IDLivro INT,
 CONSTRAINT fk_ratings_livros FOREIGN KEY (IDLivro) REFERENCES livros(IDLivro)
 );
+
+ CREATE TABLE info (
+ IDInfo INT PRIMARY KEY AUTO_INCREMENT,
+ numPaginas INT,
+ precoLivro DECIMAL (10, 2),
+ IDLivro INT,
+ CONSTRAINT fk_info_livros FOREIGN KEY (IDLivro) REFERENCES livros(IDLivro)
+ );
