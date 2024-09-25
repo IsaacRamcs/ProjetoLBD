f21030cb0d76a92051ce21b9faa5b1f76d2beec3
IsaacRamcs <142359173+IsaacRamcs@users.noreply.github.com>
2024-09-25 14:13:18 -0300

    Update consulta1.sql

--- consulta1.sql
+++ consulta1.sql
@@ -1,11 +1,5 @@
-SELECT 
-livros.categoriaLivro, 
-AVG(info.numPaginas) AS mediaPaginas
-FROM 
-livros
-JOIN 
-info ON livros.IDLivro = info.IDLivro
-GROUP BY 
-livros.categoriaLivro
-ORDER BY 
-mediaPaginas DESC;
+SELECT livros.categoriaLivro, AVG(info.numPaginas) AS mediaPaginas
+FROM livros
+JOIN info ON livros.IDLivro = info.IDLivro
+GROUP BY livros.categoriaLivro
+ORDER BY mediaPaginas DESC;
