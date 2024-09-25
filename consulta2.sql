1912f77990c595b8b86482e329a2ca838948404a
Thaboll <127352652+Thaboll@users.noreply.github.com>
2024-09-25 14:15:01 -0300

    Update consulta2.sql

--- consulta2.sql
+++ consulta2.sql
@@ -1,1 +1,7 @@
+SELECT livros.nomeLivro, autor.nomeAutor, AVG(info.numPaginas) AS Paginas
+FROM livros
+JOIN info ON livros.IDLivro = info.IDLivro
+JOIN autor ON livros.IDAutor = autor.IDAutor
+GROUP BY livros.nomeLivro, autor.nomeAutor
+ORDER BY Paginas DESC;
 
