2117be21a53d31b06d865af14484a0a5f967b1a8
IsaacRamcs <142359173+IsaacRamcs@users.noreply.github.com>
2024-09-25 22:02:19 -0300

    Update consulta3.sql

--- consulta3.sql
+++ consulta3.sql
@@ -1,12 +1,5 @@
-SELECT 
-livros.nomeLivro, 
-autor.nomeAutor, 
-ratings.notaRating
-FROM 
-livros
-JOIN 
-autor ON livros.IDAutor = autor.IDAutor
-JOIN 
-ratings ON livros.IDLivro = ratings.IDLivro
-WHERE 
-ratings.notaRating > 4.5;
+SELECT livros.nomeLivro, autor.nomeAutor, ratings.notaRating
+FROM livros
+JOIN autor ON livros.IDAutor = autor.IDAutor
+JOIN ratings ON livros.IDLivro = ratings.IDLivro
+WHERE ratings.notaRating > 4.5;
